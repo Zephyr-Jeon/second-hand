0a59ecf15e5e6c55eef4e1c065de23f7a8a74596
Zephyr.Jeon <zephyr.jeon@odcode.com>
2023-02-23 05:28:01 -0800

    infra: add TypeORM and connect server with db

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,42 @@
 import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 import { app } from './app';
 
-const start = async () => {
+const dataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'password',
+  database: 'postgres',
+  synchronize: true,
+  // entities: ['src/module/**/entities/*'],
+});
+
+const startServer = async () => {
   console.log('Starting up.....');
 
   app.get('/test', (req, res) => {
     res.send('Test!');
   });
 
+  let retries = 5;
+  while (retries) {
+    try {
+      await dataSource.initialize();
+      console.log('Data Source has been initialized!');
+      break;
+    } catch (err) {
+      console.error('Error during Data Source initialization', err);
+      retries = retries - 1;
+      console.log(`retries left ${retries}`);
+      await new Promise((res) => setTimeout(res, 3000));
+    }
+  }
+
   app.listen({ port: 3000 }, () =>
     console.log(`🚀 Server ready at http://localhost:3000`)
   );
 };
 
-start();
+startServer();
