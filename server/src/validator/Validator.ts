f45f7e12d87f034d6db8c99824d5f3cac475718a
Zephyr.Jeon <zephyr.jeon@odcode.com>
2023-03-07 16:29:58 -0800

    server: handle input validation error

--- server/src/validator/Validator.ts
+++ server/src/validator/Validator.ts
@@ -1,3 +1,6 @@
+import { ZodError } from 'zod';
+import { ERROR_CODES } from '../error/ErrorCodes';
+import { GQLError } from '../error/GQLError';
 import { SignupInput } from '../modules/user/user.input';
 import { inputSchema } from './inputSchema';
 
@@ -6,15 +9,38 @@ export class Validator {
 
   static validateInput(input: unknown, inputType: unknown) {
     const { user } = this.inputSchema;
+
     try {
       switch (inputType) {
         case SignupInput:
           user.signup.parse(input);
           break;
         default:
+          throw new GQLError({
+            message: 'Input type undefined',
+            code: ERROR_CODES.INPUT_TYPE_UNDEFINED,
+          });
       }
     } catch (e) {
-      throw new Error('Invalid Input');
+      throw this.formatError(e);
     }
+  }
+
+  private static formatError(error: unknown) {
+    if (error instanceof GQLError) {
+      return error;
+    }
+
+    if (error instanceof ZodError) {
+      const messages = error.issues.map((issue) => issue.message);
+
+      return new GQLError({
+        message: 'Invalid input',
+        code: ERROR_CODES.INVALID_INPUT,
+        data: { messages },
+      });
+    }
+
+    return new GQLError({ message: 'Unknown input validation error' });
   }
 }
